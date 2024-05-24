f903e803ba976e529af5498ebb89474aa2b11419
boosterAlex <alex.booster.com@gmail.com>
2024-05-24 22:44:39 +0300

    fix ui style

--- web/src/pages/AboutMoive/AboutMovie.tsx
+++ web/src/pages/AboutMoive/AboutMovie.tsx
@@ -1,13 +1,14 @@
+import { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Anchor, Box, Breadcrumbs, Divider } from '@mantine/core'
 import cn from 'classnames'
-import { Anchor, Breadcrumbs, Divider } from '@mantine/core'
 
 import { useMovieQuery } from 'src/services/api/api'
 import { IMG_BASE_URL } from 'src/services/api/constants'
 import { Spinner } from 'src/shared/ui'
-import RatingIcon from 'src/shared/assets/icon/rating'
-
-import styles from './AboutMovie.module.scss'
+import { RatedModal } from 'src/features/MovieCard/RatedModal/index.ts'
+import { MovieCardData } from 'src/types/index.ts'
+import { ROUTES } from 'src/shared/consts'
 import {
     formatCurrency,
     formatDate,
@@ -15,11 +16,11 @@ import {
     formatTime,
     getYearFromDate
 } from 'src/pages/AllMovies/Filters/lib/helper.ts'
+import RatingIcon from 'src/shared/assets/icon/rating'
+import NoPoster from 'src/shared/assets/icon/NoPoster.svg'
+
+import styles from './AboutMovie.module.scss'
 import useRatedMovies from 'src/hooks/useRatedMovies'
-import { useEffect, useMemo, useState } from 'react'
-import { RatedModal } from 'src/features/MovieCard/RatedModal/index.ts'
-import { MovieCardData } from 'src/types/index.ts'
-import { ROUTES } from 'src/shared/consts'
 
 const AboutMovie = () => {
     const { id } = useParams()
@@ -104,7 +105,7 @@ const AboutMovie = () => {
     if (isLoading || isFetching) return <Spinner />
     return (
         <>
-            <Breadcrumbs>
+            <Breadcrumbs className={styles.breadCrumbs}>
                 {[
                     { title: 'Movies', href: ROUTES.MAIN },
                     { title: data?.original_title, href: '#' }
@@ -114,24 +115,30 @@ const AboutMovie = () => {
                     </Anchor>
                 ))}
             </Breadcrumbs>
-            <div className={styles.container}>
-                <div className={styles.movieContainer}>
-                    <div className={styles.imageContainer}>
-                        <img
-                            src={`${IMG_BASE_URL}${data?.poster_path}`}
-                            alt={data?.original_title}
-                            loading="lazy"
-                        />
-                    </div>
-                    <div className={styles.info}>
-                        <div>
+            <Box className={styles.container}>
+                <Box className={styles.movieContainer}>
+                    <Box className={styles.imageContainer}>
+                        {data?.poster_path ? (
+                            <img
+                                src={data?.poster_path}
+                                alt={data?.original_title}
+                                loading="lazy"
+                            />
+                        ) : (
+                            <Box className={styles.poster}>
+                                <NoPoster />
+                            </Box>
+                        )}
+                    </Box>
+                    <Box className={styles.info}>
+                        <Box>
                             <span className={styles.title}>
                                 {data?.original_title}
                             </span>
                             <span className={styles.year}>
                                 {getYearFromDate(data?.release_date)}
                             </span>
-                            <div className={styles.ratingContainer}>
+                            <Box className={styles.ratingContainer}>
                                 <RatingIcon color="#FAB005" />
                                 <span className={styles.average}>
                                     {data?.vote_average.toFixed(1)}
@@ -139,9 +146,9 @@ const AboutMovie = () => {
                                 <span
                                     className={styles.greyText}
                                 >{`(${formatNumber(data?.vote_count || '')})`}</span>
-                            </div>
-                        </div>
-                        <div className={styles.description}>
+                            </Box>
+                        </Box>
+                        <Box className={styles.description}>
                             {data && (
                                 <ul className={styles.listContainer}>
                                     {OPTIONS_LIST.map((item) => (
@@ -169,10 +176,10 @@ const AboutMovie = () => {
                                     ))}
                                 </ul>
                             )}
-                        </div>
-                    </div>
-                </div>
-                <div
+                        </Box>
+                    </Box>
+                </Box>
+                <Box
                     onClick={handleOpenModal}
                     className={styles.ratingStarContainer}
                 >
@@ -180,12 +187,12 @@ const AboutMovie = () => {
                     {getPersonalRating(id) && (
                         <span>{getPersonalRating(id)}</span>
                     )}
-                </div>
-            </div>
+                </Box>
+            </Box>
 
-            <div className={cn(styles.container, styles.columnContainer)}>
+            <Box className={cn(styles.container, styles.columnContainer)}>
                 {data?.trailerUrl && (
-                    <div className={styles.trailerContainer}>
+                    <Box className={styles.trailerContainer}>
                         <span className={styles.infoTitle}>Trailer</span>
                         <iframe
                             src={data.trailerUrl}
@@ -193,26 +200,26 @@ const AboutMovie = () => {
                             allowFullScreen
                             className={styles.trailer}
                         />
-                        <Divider my="lg" className={styles.divider} />
-                    </div>
+                        <Divider my="lg" className={styles.Boxider} />
+                    </Box>
                 )}
-                <div className={styles.descriptionContainer}>
+                <Box className={styles.descriptionContainer}>
                     <span className={styles.infoTitle}>Description</span>
-                    <div className={styles.descriptionText}>
+                    <Box className={styles.descriptionText}>
                         {data?.overview}
-                    </div>
-                    <Divider my="lg" className={styles.divider} />
-                </div>
-                <div className={styles.productionContainer}>
+                    </Box>
+                    <Divider my="lg" className={styles.Boxider} />
+                </Box>
+                <Box className={styles.productionContainer}>
                     <span className={styles.infoTitle}>Production</span>
-                    <div className={styles.production}>
+                    <Box className={styles.production}>
                         <ul className={styles.listContainer}>
                             {data?.production_companies.map((company) => (
                                 <li
                                     className={styles.productionOption}
                                     key={company.name}
                                 >
-                                    <div
+                                    <Box
                                         key={company.id}
                                         className={styles.company}
                                     >
@@ -221,16 +228,16 @@ const AboutMovie = () => {
                                             alt={company.name}
                                             loading="lazy"
                                         />
-                                    </div>
+                                    </Box>
                                     <span className={styles.companyText}>
                                         {company.name}
                                     </span>
                                 </li>
                             ))}
                         </ul>
-                    </div>
-                </div>
-            </div>
+                    </Box>
+                </Box>
+            </Box>
 
             <RatedModal
                 movieTitle={data?.original_title || 'N/A'}
--- web/src/pages/RatedMovies/RatedMovies.tsx
+++ web/src/pages/RatedMovies/RatedMovies.tsx
@@ -1,138 +1,10 @@
-// import { MovieCardData } from 'src/types'
-// import { MovieCard } from 'src/features'
-
-// import { useEffect, useState } from 'react'
-// import { Box, Button, Grid, Input, Pagination, Title } from '@mantine/core'
-
-// import DontSuch from 'src/shared/assets/icon/DontSuch.svg'
-
-// const MAX_MOVIES_ON_PAGE = 4
-
-// import styles from './RatedMovies.module.scss'
-
-// function filterSearch(text: string, query: string): boolean {
-//     return text.toLowerCase().includes(query.toLowerCase())
-// }
-
-// const RatedMovies = () => {
-//     const [allMovies, setAllMovies] = useState<MovieCardData[]>(
-//         JSON.parse(localStorage.getItem('ratedMovies') as string)
-//     )
-//     const [currentPage, setCurrentPage] = useState(1)
-//     const [filter, setFilter] = useState('')
-
-//     const currentMovies = allMovies.slice(
-//         (currentPage - 1) * MAX_MOVIES_ON_PAGE,
-//         currentPage * MAX_MOVIES_ON_PAGE
-//     )
-
-//     useEffect(() => {
-//         if (filter.trim() === '') {
-//             setAllMovies(
-//                 JSON.parse(localStorage.getItem('ratedMovies') as string)
-//             )
-//         }
-//     }, [filter])
-
-//     const handleRemove = (data: MovieCardData[]) => {
-//         const actualTotalPages = Math.ceil(data.length / MAX_MOVIES_ON_PAGE)
-
-//         if (currentPage > actualTotalPages) {
-//             setCurrentPage(actualTotalPages)
-//         }
-//         setAllMovies(data)
-//     }
-
-//     const randerPagination = () => {
-//         if (allMovies.length <= 4) {
-//             return null
-//         }
-//         return (
-//             <Box className={styles.pagination}>
-//                 <Pagination
-//                     total={Math.ceil(allMovies.length / MAX_MOVIES_ON_PAGE)}
-//                     value={currentPage}
-//                     onChange={(val) => {
-//                         setCurrentPage(val)
-//                     }}
-//                     mt="sm"
-//                 />
-//             </Box>
-//         )
-//     }
-
-//     const onSearch = () => {
-//         if (filter.trim() === '') {
-//             setAllMovies(
-//                 JSON.parse(localStorage.getItem('ratedMovies') as string)
-//             )
-//         } else {
-//             setAllMovies((pre) => {
-//                 return pre.filter((item) =>
-//                     filterSearch(item.original_title, filter)
-//                 )
-//             })
-//         }
-//     }
-
-//     return (
-//         <>
-//             <Box className={styles.wrapperHeader}>
-//                 <Title fz="32px" fw="700" lh="150%" lts="2">
-//                     Rated movies
-//                 </Title>
-//                 <Box className={styles.wrapperInput}>
-//                     <Input
-//                         size="md"
-//                         className={styles.input}
-//                         value={filter}
-//                         onChange={(event) => setFilter(event.target.value)}
-//                         placeholder="Search movie title"
-//                     />
-//                     <Button
-//                         className={styles.searchButton}
-//                         size="xs"
-//                         onClick={onSearch}
-//                     >
-//                         Search
-//                     </Button>
-//                 </Box>
-//             </Box>
-//             {allMovies.length === 0 && (
-//                 <Box maw="1440px" m="0 auto" className={styles.emptyData}>
-//                     <DontSuch />
-//                     <span style={{ marginTop: '40px' }}>
-//                         We don't have such movies, look for another one
-//                     </span>
-//                 </Box>
-//             )}
-//             <Box className={styles.wrapperMovies}>
-//                 <Grid columns={2}>
-//                     {currentMovies.map((movie: MovieCardData) => {
-//                         return (
-//                             <MovieCard
-//                                 key={movie.id}
-//                                 currentMovie={movie}
-//                                 handleRemoveFromFavourite={(data) =>
-//                                     handleRemove(data)
-//                                 }
-//                             />
-//                         )
-//                     })}
-//                 </Grid>
-//             </Box>
-//             {randerPagination()}
-//         </>
-//     )
-// }
-
-// export default RatedMovies
 import { MovieCardData } from 'src/types'
 import { MovieCard } from 'src/features'
 import { useEffect, useState } from 'react'
 import { Box, Button, Grid, Input, Pagination, Title } from '@mantine/core'
 
 import DontSuch from 'src/shared/assets/icon/DontSuch.svg'
+import EmptyState from 'src/shared/assets/icon/EmptyState.svg'
 
 import styles from './RatedMovies.module.scss'
 
@@ -146,6 +18,7 @@ const RatedMovies = () => {
     const [allMovies, setAllMovies] = useState<MovieCardData[]>(
         JSON.parse(localStorage.getItem('ratedMovies') as string)
     )
+
     const [currentPage, setCurrentPage] = useState(1)
     const [filter, setFilter] = useState('')
 
@@ -191,62 +64,79 @@ const RatedMovies = () => {
 
     return (
         <>
-            <Box className={styles.wrapperHeader}>
-                <Title fz="32px" fw="700" lh="150%" lts="2">
-                    Rated movies
-                </Title>
-                <Box className={styles.wrapperInput}>
-                    <Input
-                        size="md"
-                        className={styles.input}
-                        value={filter}
-                        onChange={(event) =>
-                            setFilter(event.target.value.trim())
-                        }
-                        placeholder="Search movie title"
-                    />
-                    <Button
-                        className={styles.searchButton}
-                        size="xs"
-                        onClick={onSearch}
-                    >
-                        Search
-                    </Button>
-                </Box>
-            </Box>
-            {allMovies.length === 0 && (
+            {allMovies.length === 0 ? (
                 <Box maw="1440px" m="0 auto" className={styles.emptyData}>
-                    <DontSuch />
+                    <EmptyState />
                     <span style={{ marginTop: '40px' }}>
-                        We don't have such movies, look for another one
+                        You haven't rated any films yet
                     </span>
                 </Box>
-            )}
-            <Box className={styles.wrapperMovies}>
-                <Grid columns={2}>
-                    {currentMovies.map((movie: MovieCardData) => {
-                        return (
-                            <MovieCard
-                                key={movie.id}
-                                currentMovie={movie}
-                                handleRemoveFromFavourite={(data) =>
-                                    handleRemove(data)
+            ) : (
+                <Box>
+                    <Box className={styles.wrapperHeader}>
+                        <Title fz="32px" fw="700" lh="150%" lts="2">
+                            Rated movies
+                        </Title>
+                        <Box className={styles.wrapperInput}>
+                            <Input
+                                size="md"
+                                className={styles.input}
+                                value={filter}
+                                onChange={(event) =>
+                                    setFilter(event.target.value.trim())
                                 }
+                                placeholder="Search movie title"
                             />
-                        )
-                    })}
-                </Grid>
-            </Box>
-            {allMovies.length > MAX_MOVIES_ON_PAGE && (
-                <Box className={styles.pagination}>
-                    <Pagination
-                        total={Math.ceil(allMovies.length / MAX_MOVIES_ON_PAGE)}
-                        value={currentPage}
-                        onChange={(val) => {
-                            setCurrentPage(val)
-                        }}
-                        mt="sm"
-                    />
+                            <Button
+                                className={styles.searchButton}
+                                size="xs"
+                                onClick={onSearch}
+                            >
+                                Search
+                            </Button>
+                        </Box>
+                    </Box>
+                    {currentMovies.length === 0 && !allMovies.length && (
+                        <Box
+                            maw="1440px"
+                            m="0 auto"
+                            className={styles.emptyData}
+                        >
+                            <DontSuch />
+                            <span style={{ marginTop: '40px' }}>
+                                We don't have such movies, look for another one
+                            </span>
+                        </Box>
+                    )}
+                    <Box className={styles.wrapperMovies}>
+                        <Grid columns={2}>
+                            {currentMovies.map((movie: MovieCardData) => {
+                                return (
+                                    <MovieCard
+                                        key={movie.id}
+                                        currentMovie={movie}
+                                        handleRemoveFromFavourite={(data) =>
+                                            handleRemove(data)
+                                        }
+                                    />
+                                )
+                            })}
+                        </Grid>
+                    </Box>
+                    {allMovies.length > MAX_MOVIES_ON_PAGE && (
+                        <Box className={styles.pagination}>
+                            <Pagination
+                                total={Math.ceil(
+                                    allMovies.length / MAX_MOVIES_ON_PAGE
+                                )}
+                                value={currentPage}
+                                onChange={(val) => {
+                                    setCurrentPage(val)
+                                }}
+                                mt="sm"
+                            />
+                        </Box>
+                    )}
                 </Box>
             )}
         </>
